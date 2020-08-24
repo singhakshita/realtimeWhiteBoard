1a555b01c824b9dae43f517c719df8bcbfc1045d
singhakshita <52077474+singhakshita@users.noreply.github.com>
2020-08-24 18:09:20 +0530

    test

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ io.on("connection" , function(socket){
         socket.broadcast.emit("onmm" ,point);
     })
 })
-// app.get("home" , function(req ,res){
-//     res.end("<h1>welcome to home page</h1>")
-// })
+app.get("/home" , function(req ,res){
+    res.end("<h1>welcome to home page</h1>")
+})
 app.use(express.static('client'));
 
 let port = process.env.PORT || 3000;
